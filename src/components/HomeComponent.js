7a0d8a37620633acba133ec596cae7bb3ae84ec8
Chidiebere Nwabunike <samchidi2005@yahoo.com>
2020-04-09 01:00:03 -0500

    React Transitions

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -16,7 +16,7 @@ function RenderCard({item, isLoading, errMess}) {
     } 
     return (
         <Card>
-           <CardImg src={baseUrl + item.image} alt={item.name} />
+            <CardImg src={baseUrl + item.image} alt={item.name} />
             <CardBody>
                 <CardTitle>{item.name}</CardTitle>
                 <CardText>{item.description}</CardText>
