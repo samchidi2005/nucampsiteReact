d87fb14f5380d126f32979048618ac817a728f80
Chidiebere Nwabunike <samchidi2005@yahoo.com>
2020-04-08 22:24:16 -0500

    Fetch Handling Errors

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 
@@ -16,11 +17,7 @@ function RenderCard({item, isLoading, errMess}) {
     } 
     return (
         <Card>
-            <CardImg src={baseUrl + item.image} alt={item.name} />
-            <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                <CardText>{item.description}</CardText>
-            </CardBody>
+            
         </Card>
     );
 }
